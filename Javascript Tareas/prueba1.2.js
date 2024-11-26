89b07bffad2251f42929e764561147b8224ae0cf
Javiiidh <javiiidh@gmail.com>
2024-11-26 14:21:27 +0100

    eg

--- Javascript Tareas/prueba1.2.js
+++ Javascript Tareas/prueba1.2.js
@@ -5,3 +5,8 @@ document.write("<table border=\"1\">");
 document.write("<tr><th>Nombre</th><th>Instituto</th></tr>");
 document.write("<tr><td>" + nombre + "</td><td>" + instituto + "</td></tr>");
 document.write("</table>");
+
+var nota1 = 2;
+var nota2 = 4;
+var notalTotal = nota1 + nota2;
+var notaTotal2 = (nota1 + nota2) / 2;
