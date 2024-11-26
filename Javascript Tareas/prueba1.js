6ec6c36a1d1b4829f9dd58fcf6e75d7b1562c067
Javiiidh <javiiidh@gmail.com>
2024-11-26 13:44:19 +0100

    Java

---
+++ Javascript Tareas/prueba1.js
@@ -0,0 +1,20 @@
+console.log("prueba de codigo")
+
+function ImprimirMensaje(mensaje) {
+    console.log(mensaje);
+    //alert(mensaje);
+    console.log("hola mundo");
+}
+
+function TipoDeVariable(mivariable) {
+    console.log("Mi variable es de tipo" + typeof(mivariable));
+}
+
+var alumnofila2 = ["Jacob", "Aleks", "David", "Jorge"];
+TipoDeVariable(alumnofila2);
+
+var notaExamen3 = 6;
+TipoDeVariable(notaExamen3);
+
+var miNombre = "Javier";
+TipoDeVariable(miNombre);
