e21b1f7d60a24eeaad2155f8354f8df61d1765b8
Javiiidh <javiiidh@gmail.com>
2024-11-26 14:11:54 +0100

    Js

--- Javascript Tareas/prueba1.2.js
+++ Javascript Tareas/prueba1.2.js
@@ -1,4 +1,7 @@
 var nombre = prompt("Dime tu nombre");
-console.log(nombre);
-alert(nombre);
-document.write("<h1>" + nombre + "</h1>");
+var instituto = prompt("Nombre de Instituto")
+
+document.write("<table border=\"1\">");
+document.write("<tr><th>Nombre</th><th>Instituto</th></tr>");
+document.write("<tr><td>" + nombre + "</td><td>" + instituto + "</td></tr>");
+document.write("</table>");
