9031bdba42d9232d24150ad04cefc1e9230befdd
Javiiidh <javiiidh@gmail.com>
2024-11-28 13:01:46 +0100

    sd

--- Javascript Ejemplos/prueba  1.2.js
+++ Javascript Ejemplos/prueba  1.2.js
@@ -5,13 +5,34 @@ function mostrarFecha(){
     document.write("<h1>" + now + "</h1>");
 }
 
+//llevar a la funcion mostrarfecha cuando hago click en el boton boton1
 document.getElementById("boton1").onclick = () => {
     console.log("Click sobre el boton 1...");
     mostrarFecha();
 }
 
+//cambiar estilos de div con id micaja cuando paso el ratón por encima
 var micaja = document.getElementById("micaja");
 micaja.onmouseover = () => {
     micaja.style.backgroundColor = "green";
     micaja.style.fontSize = "40px";
+}
+
+//cambiar texto del div con id mi caja cuando salgo de encima del elemento
+micaja.onmouseleave = () => {
+    micaja.innerHTML += "hasta luego"
+}
+
+
+document.getElementById("micaja").style.fontSize = "10px";
+
+function changefontsize () {
+    var micaja = document.getElementById("micaja");
+    var fontSizemicaja = micaja.style.fontSize;
+    console.log("mi caja font size" + fontSizemicaja);
+}
+
+var changefontsize = document.getElementById("changefontsize");
+changefontsize.onclick = () => {
+    changefontsize();
 }
